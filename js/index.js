a0a9a21145bb9a245ac555613a138709a7220333
Julio99 <Julioespinola9999@gmail.com>
2022-06-01 16:46:33 -0400

    commit9

--- js/index.js
+++ js/index.js
@@ -5,5 +5,5 @@ const d = document;
 
 d.addEventListener('click', (e)=>{
     modal(e, '.modal-rules__icon', '.show-modal', '.modal-rules');
-    options(e, '.options')
+    options(e, '.options-container', '.options-container__item')
 })
--- js/options.js
+++ js/options.js
@@ -2,33 +2,41 @@ const d = document;
 
 
 
-export default function options(e, option){
+export default function options(e, option_container, option){
 
-    let $oldChild = d.querySelector(option),
+    let $oldChild = d.querySelector(option_container),
     $newChild = d.importNode($oldChild),
-    $options = d.querySelectorAll('.wrapper');
+    $options = d.querySelectorAll('.options-container__item');
     $newChild.style.setProperty('background-image', 'none');
-
+    
     console.log($options);
 
     if(e.target.matches(`${option} *`)){
         let $node;
-        if(e.target.matches('.options__item')){
+        if(e.target.matches('.wrapper')){
             $node = d.importNode(e.target.parentElement, true);
-        }else{
-            $node = d.importNode(e.target, true);
+        }else if(e.target.matches('.options__item')){
+            $node = d.importNode(e.target.parentElement.parentElement, true);
         }
-
+    
+        $node.querySelector('.options__picked').style.setProperty('display', 'inline')
         $node.style.setProperty('grid-column', '1/2')
-        $newChild.appendChild($node);
-
+            
+    
         let numero = Math.round(Math.random() * (2));
         d.querySelector('.container').replaceChild($newChild, $oldChild);
+        $newChild.appendChild($node);
+        
+            
         $node = d.importNode($options[numero], true)
         $node.style.setProperty('grid-column', '2/-1')
+        $node.querySelector('.options__house').style.setProperty('display', 'inline')
         setTimeout(()=>{
             $newChild.appendChild($node)
         },1000)
-    }
 
+        
+
+        $newChild.insertAdjacentHTML('afterend', '<span>Hola</span>')
+    }
 }
